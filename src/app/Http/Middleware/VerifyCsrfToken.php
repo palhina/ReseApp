43d40a192544006f6e0c7206a762201d97ca2f0f
hitomi <pal96894089@gmail.com>
2024-04-15 11:59:26 +0900

    PHPUnitテスト実装

--- src/app/Http/Middleware/VerifyCsrfToken.php
+++ src/app/Http/Middleware/VerifyCsrfToken.php
@@ -6,6 +6,16 @@ use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
 
 class VerifyCsrfToken extends Middleware
 {
+    public function handle($request, \Closure $next)
+    {
+        if(env('APP_ENV') !== 'testing')
+        {
+            return parent::handle($request, $next);
+        }
+
+        return $next($request);
+    }
+    
     /**
      * The URIs that should be excluded from CSRF verification.
      *
